74b32aa45ece46e0c5d2a03aa652c688ee82ebf5
Jacky Yang <menglinyang@MenglindeMacBook-Pro.local>
2022-09-07 16:54:41 +0800

    Create - project

---
+++ Essentail-practice/Essentail_practice.h
@@ -0,0 +1,18 @@
+//
+//  Essentail_practice.h
+//  Essentail-practice
+//
+//  Created by Menglin Yang on 2022/9/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Essentail_practice.
+FOUNDATION_EXPORT double Essentail_practiceVersionNumber;
+
+//! Project version string for Essentail_practice.
+FOUNDATION_EXPORT const unsigned char Essentail_practiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Essentail_practice/PublicHeader.h>
+
+

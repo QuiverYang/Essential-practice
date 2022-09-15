4c438992e4f8a1d9f6bca7309e0987af23351cf4
Jacky Yang <menglinyang@MenglindeMacBook-Pro.local>
2022-09-15 15:08:58 +0800

    Create FeedItem model

---
+++ Essentail-practice/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Essentail-practice
+//
+//  Created by Menglin Yang on 2022/9/15.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id : UUID
+    let description : String?
+    let location : String?
+    let imageURL : URL
+}

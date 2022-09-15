d20ccd5d9b4be4b4d8d2899f1dfb05cf556a0619
Jacky Yang <menglinyang@MenglindeMacBook-Pro.local>
2022-09-15 15:10:57 +0800

    Create the FeedLoader protocol boundary

---
+++ Essentail-practice/Feed Feature/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  Essentail-practice
+//
+//  Created by Menglin Yang on 2022/9/15.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+protocol FeedLoader {
+    func load(completion : @escaping (LoadFeedResult) -> Void)
+}

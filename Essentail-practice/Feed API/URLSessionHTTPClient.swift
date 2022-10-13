4d25fc2d8ae9188b37015905a6a825f8a889a330
Jacky Yang <m778677@yahoo.com.tw>
2022-10-13 13:49:51 +0800

    add URLSession as constructor injection and giving the default global value

--- Essentail-practice/Feed API/URLSessionHTTPClient.swift
+++ Essentail-practice/Feed API/URLSessionHTTPClient.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 public class URLSessionHTTPClient: HttpClient {
-    public init() {}
+    private let session: URLSession
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
     
     private struct UnexpectedValuesRepresentation: Error {}
     
     public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
             } else if let data = data, let response = response as? HTTPURLResponse{

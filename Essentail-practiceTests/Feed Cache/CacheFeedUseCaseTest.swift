704dbe4aba7cddf6e6ba5ba4aa0f01c864dc3ebd
Jacky Yang <m778677@yahoo.com.tw>
2022-10-19 11:58:29 +0800

    Add memory leak tracking

--- Essentail-practiceTests/Feed Cache/CacheFeedUseCaseTest.swift
+++ Essentail-practiceTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -40,9 +40,11 @@ class CacheFeedUseCaseTest: XCTestCase {
     
     //MARK: - Helpers
     
-    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        trackFroMemoryLeaks(store, file: file, line: line)
+        trackFroMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
     

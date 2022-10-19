7d4fe20111807802b94decdbd542122f512ba3bb
Jacky Yang <m778677@yahoo.com.tw>
2022-10-19 11:49:29 +0800

    Save command request cache deletion

--- Essentail-practiceTests/Feed Cache/CacheFeedUseCaseTest.swift
+++ Essentail-practiceTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -6,10 +6,16 @@
 //
 
 import XCTest
+import Essentail_practice
 
 class LocalFeedLoader {
+    private let store : FeedStore
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeedCallCount += 1
     }
 }
 
@@ -24,5 +30,25 @@ class CacheFeedUseCaseTest: XCTestCase {
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
     }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    //MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem{
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "https://some-url")!
+    }
+    
 
 }

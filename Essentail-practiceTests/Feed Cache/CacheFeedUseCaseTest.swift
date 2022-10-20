eeca81c345f510dee72a9bad942a2f9b48bf5e37
Jacky Yang <m778677@yahoo.com.tw>
2022-10-20 16:54:52 +0800

    Extract ˋFeedStoreˋ protocol from test spy helper

--- Essentail-practiceTests/Feed Cache/CacheFeedUseCaseTest.swift
+++ Essentail-practiceTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -28,9 +28,15 @@ class LocalFeedLoader {
     }
 }
 
-class FeedStore {
+protocol FeedStore {
     typealias DeletionCompletion = (Error?)->Void
     typealias InsertionCompletion = (Error?)->Void
+    func deleteCacheFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
+class FeedStoreSpy : FeedStore {
+    
 
     enum ReceivedMessage : Equatable{
         case deletionCacheFeed
@@ -140,8 +146,8 @@ class CacheFeedUseCaseTest: XCTestCase {
     
     //MARK: - Helpers
     
-    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
-        let store = FeedStore()
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
         let sut = LocalFeedLoader(store: store, currentDate: currentDate)
         trackFroMemoryLeaks(store, file: file, line: line)
         trackFroMemoryLeaks(sut, file: file, line: line)

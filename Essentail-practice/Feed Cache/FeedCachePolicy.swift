a9b49307372abd0d861fee1fb586b3d7d38ede1c
Jacky Yang <m778677@yahoo.com.tw>
2022-11-18 10:42:42 +0800

    Move ˋFeedCachePolicyˋ to tis own file

---
+++ Essentail-practice/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCachePolicy.swift
+//  Essentail-practice
+//
+//  Created by Menglin Yang on 2022/11/18.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDay : Int { return 7}
+    
+    internal static func validate(_ timestamp: Date, against date : Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDay, to: timestamp) else {return false}
+        return date < maxCacheAge
+    }
+}

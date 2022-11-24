886767133f87ae4ab28f5bbedac576d246a68187
Jacky Yang <m778677@yahoo.com.tw>
2022-11-24 16:23:02 +0800

    Extract duplicate insert test code into a reusable helper method

--- Essentail-practiceTests/Feed Cache/CodableFeedStoreTests.swift
+++ Essentail-practiceTests/Feed Cache/CodableFeedStoreTests.swift
@@ -80,12 +80,7 @@ class CodableFeedStoreTests : XCTestCase {
         let feed = uniqueImageFeed().local
         let timestamp = Date()
         
-        let exp = expectation(description: "Wait for cache retrieval")
-        sut.insert(feed, timestamp: timestamp, completion: { insertionError in
-            XCTAssertNil(insertionError)
-            exp.fulfill()
-        })
-        wait(for: [exp], timeout: 1.0)
+        insert((feed, timestamp), to: sut)
         
         expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
 
@@ -93,15 +88,9 @@ class CodableFeedStoreTests : XCTestCase {
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache retrieval")
         let feed = uniqueImageFeed().local
         let timestamp = Date()
-        
-        sut.insert(feed, timestamp: timestamp, completion: { insertionError in
-            XCTAssertNil(insertionError)
-            exp.fulfill()
-        })
-        wait(for: [exp], timeout: 1.0)
+        insert((feed, timestamp), to: sut)
         
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
@@ -134,6 +123,15 @@ class CodableFeedStoreTests : XCTestCase {
     private func expect(_ sut: CodableFeedStore, toRetrieveTwice expectedResult: RetriveCacheFeedResult, file: StaticString = #filePath, line: UInt = #line) {
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
+    }
+    
+    private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for cache insertion")
+        sut.insert(cache.feed, timestamp: cache.timestamp, completion: { insertionError in
+            XCTAssertNil(insertionError, file: file, line: line)
+            exp.fulfill()
+        })
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func testSpecificStoreURL() -> URL {

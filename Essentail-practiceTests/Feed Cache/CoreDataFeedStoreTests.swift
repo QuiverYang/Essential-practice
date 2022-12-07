1ecfbb5d9ed1933f85ecd0dcce982e098d254985
Jacky Yang <m778677@yahoo.com.tw>
2022-12-07 15:06:27 +0800

    `CoreDataFeedStore.insert()` delivers no error on empty cache

--- Essentail-practiceTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ Essentail-practiceTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -36,7 +36,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatInsertionDeleversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {

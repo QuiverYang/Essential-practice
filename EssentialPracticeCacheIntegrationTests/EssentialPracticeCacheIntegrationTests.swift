626f7a7904ffdefd80fa31db052c8916873d434e
Jacky Yang <m778677@yahoo.com.tw>
2022-12-17 21:59:25 +0800

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` delivers items saved on separate instances, proving we correctly persist the data models to disk.

--- EssentialPracticeCacheIntegrationTests/EssentialPracticeCacheIntegrationTests.swift
+++ EssentialPracticeCacheIntegrationTests/EssentialPracticeCacheIntegrationTests.swift
@@ -22,7 +22,7 @@ final class EssentialPracticeCacheIntegrationTests: XCTestCase {
         let exp = expectation(description: "Wait for loading")
         sut.load { result in
             switch result {
-            case let .success(imageFeed):
+            case let .success(imageFeed):`
                 XCTAssertEqual(imageFeed, [], "expected empty Feed")
             case let .failure(error):
                 XCTFail("Expected successful feed result, got \(error) instead")
@@ -32,6 +32,32 @@ final class EssentialPracticeCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
     }
+    func test_load_deliversItemsSavedOnASeparateInstance(){
+            let sutToPerformSave = makeSUT()
+            let sutToPerformLoad = makeSUT()
+            let feed = uniqueImageFeed().models
+            
+            let saveExp = expectation(description: "Wait to save")
+            sutToPerformSave.save(feed) { savedError in
+                XCTAssertNil(savedError, "Expected to save feed successfully")
+                saveExp.fulfill()
+            }
+            wait(for: [saveExp], timeout: 1.0)
+            
+            let loadExp = expectation(description: "Wait for load completion")
+            sutToPerformLoad.load { result in
+                switch result {
+                case let .success(imageFeed):
+                    XCTAssertEqual(imageFeed, feed, "expected empty Feed")
+                case let .failure(error):
+                    XCTFail("Expected successful feed result, got \(error) instead")
+                }
+                loadExp.fulfill()
+            }
+            wait(for: [loadExp], timeout: 1.0)
+            
+        }
+
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)

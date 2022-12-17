95a37248402e2876e1041e567d06017941964036
Jacky Yang <m778677@yahoo.com.tw>
2022-12-17 13:45:50 +0800

    Add `EssentialFeedCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialPracticeCacheIntegrationTests/EssentialPracticeCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialPracticeCacheIntegrationTests.swift
+//  EssentialPracticeCacheIntegrationTests
+//
+//  Created by Menglin Yang on 2022/12/17.
+//
+
+import XCTest
+
+final class EssentialPracticeCacheIntegrationTests: XCTestCase {
+
+
+}

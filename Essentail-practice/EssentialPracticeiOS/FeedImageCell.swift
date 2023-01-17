b555281a4e3ce58bb840b831bf3655eea2d98d22
Jacky Yang <m778677@yahoo.com.tw>
2023-01-17 22:43:41 +0800

    Render loaded feed on successful load completion

---
+++ Essentail-practice/EssentialPracticeiOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialPracticeiOS
+//
+//  Created by Menglin Yang on 2023/1/17.
+//
+
+import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}

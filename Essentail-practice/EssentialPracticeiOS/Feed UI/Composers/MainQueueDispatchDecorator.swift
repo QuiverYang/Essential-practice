70882cb1f0fe237041c1bb28fc8005a92a630bde
Jacky Yang <m778677@yahoo.com.tw>
2023-04-10 15:12:47 +0800

    Move `MainQueueDispatchDecorator` to separate file

---
+++ Essentail-practice/EssentialPracticeiOS/Feed UI/Composers/MainQueueDispatchDecorator.swift
@@ -0,0 +1,49 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  EssentialPracticeiOS
+//
+//  Created by Menglin Yang on 2023/4/10.
+//
+
+import Foundation
+import Essentail_practice
+
+class MainThreadDispatchDecorator<T>{
+    let decoratee: T
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        completion()
+    }
+
+    
+}
+
+extension MainThreadDispatchDecorator: FeedLoader where T == FeedLoader {
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            guard self != nil else { return }
+            self?.dispatch{
+                completion(result)
+            }
+        }
+    }
+}
+
+extension MainThreadDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        decoratee.loadImageData(from: url) { [weak self] result in
+            guard self != nil else { return }
+            self?.dispatch{
+                completion(result)
+            }
+        }
+    }
+
+    
+}

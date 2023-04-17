b0b2ba870be624e929b861db94d641a0181c9e23
Jacky Yang <m778677@yahoo.com.tw>
2023-04-17 16:40:40 +0800

    Add localized `FeedPresenter.title`

--- Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
+++ Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
@@ -20,6 +20,13 @@ class FeedPresenter  {
     private let loadingView: FeedLoadingView
     private let feedView: FeedView
     
+    static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Title for the feed view")
+    }
+    
     static var errorMessage: String {
         return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
                                  tableName: "Feed",
@@ -134,6 +141,11 @@ final class FeedPresenterTest: XCTestCase {
         }
         
     }
+    
+    func test_title_isLocalized() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
+    
     
     
     // Helpers:

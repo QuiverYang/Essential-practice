6c42fca69c3593bb0219a9627115e64f0257428e
Jacky Yang <m778677@yahoo.com.tw>
2023-04-17 15:43:32 +0800

    Extract system under test creation to a factory helper function

--- Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
+++ Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
@@ -17,15 +17,26 @@ class FeedPresenter  {
 final class FeedPresenterTest: XCTestCase {
 
     func test_FeedDoesNotSendMessagesToView() {
-        let view = ViewSpy()
-        
-        let _ = FeedPresenter(view: view)
+        let (_, view) = makeSUT()
+
         
         XCTAssertTrue(view.messages.isEmpty, "Expect no view message")
     }
     
     private class ViewSpy {
         var messages = [Any]()
+    }
+    
+    // Helpers:
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let presenter = FeedPresenter(view: view)
+        trackForMemoryLeaks(presenter)
+        trackForMemoryLeaks(view)
+        return (presenter, view)
+    
+        
     }
 
 }

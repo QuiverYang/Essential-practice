bc371670e21525696e7a90400d4105fee6b5e015
Jacky Yang <m778677@yahoo.com.tw>
2023-04-17 15:54:21 +0800

    ˋFeedPresenterˋ displays no error on ˋdidStartLoadingFeedˋ

--- Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
+++ Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
@@ -8,9 +8,27 @@
 import XCTest
 
 class FeedPresenter  {
-    init(view: Any) {
-        
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
     }
+    
+    var errorView: FeedErrorView
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError())
+    }
+}
+
+struct FeedErrorViewModelData {
+    var message: String?
+    
+    static func noError() -> FeedErrorViewModelData {
+        return FeedErrorViewModelData(message: .none)
+    }
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModelData)
 }
 
 
@@ -23,15 +41,31 @@ final class FeedPresenterTest: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expect no view message")
     }
     
-    private class ViewSpy {
-        var messages = [Any]()
+    func test_didStartLoadingFeed_displayNoErrorMessage() {
+        let (presneter, view) = makeSUT()
+        
+        presneter.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage:.none)])
+    }
+    
+    private class ViewSpy: FeedErrorView {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        func display(_ viewModel: FeedErrorViewModelData) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
+
     }
     
     // Helpers:
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let presenter = FeedPresenter(view: view)
+        let presenter = FeedPresenter(errorView: view)
         trackForMemoryLeaks(presenter)
         trackForMemoryLeaks(view)
         return (presenter, view)

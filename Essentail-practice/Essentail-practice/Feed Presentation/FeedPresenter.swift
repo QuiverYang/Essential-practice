c2c731beacfda71d9b44fa9ab5e2fc59f23a0f67
Jacky Yang <m778677@yahoo.com.tw>
2023-04-17 16:52:43 +0800

    Move prod types to the EssentialFeed production module

---
+++ Essentail-practice/Essentail-practice/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,82 @@
+//
+//  FeedPresenter.swift
+//  Essentail-practice
+//
+//  Created by Menglin Yang on 2023/4/17.
+//
+
+import Foundation
+
+public struct FeedErrorViewModelData {
+    public var message: String?
+    
+    public static func noError() -> FeedErrorViewModelData {
+        return FeedErrorViewModelData(message: .none)
+    }
+    
+    public static func error(message: String) -> FeedErrorViewModelData {
+        return FeedErrorViewModelData(message: message)
+    }
+}
+
+public protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModelData)
+}
+
+public struct FeedLoadingViewModelData {
+    public let isLoading: Bool
+}
+
+public protocol FeedLoadingView{
+    func display(_ viewModel: FeedLoadingViewModelData)
+}
+
+public struct FeedViewModelData {
+    public let feed: [FeedImage]
+}
+
+public protocol FeedView {
+    func display(_ viewModel: FeedViewModelData)
+}
+
+public final class FeedPresenter  {
+    public init(errorView: FeedErrorView, loadingView: FeedLoadingView, feedView: FeedView) {
+        self.errorView = errorView
+        self.loadingView = loadingView
+        self.feedView = feedView
+        
+    }
+    
+    private let errorView: FeedErrorView
+    private let loadingView: FeedLoadingView
+    private let feedView: FeedView
+    
+    public static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Title for the feed view")
+    }
+    
+    public static var errorMessage: String {
+        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "load error message")
+    }
+    
+    public func didStartLoadingFeed() {
+        errorView.display(.noError())
+        loadingView.display(FeedLoadingViewModelData(isLoading: true))
+    }
+    
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModelData(feed: feed))
+        loadingView.display(FeedLoadingViewModelData(isLoading: false))
+    }
+    
+    public func didFinsihLoadingFeed(with error: Error) {
+        loadingView.display(FeedLoadingViewModelData(isLoading: false))
+        errorView.display(.error(message: FeedPresenter.errorMessage))
+    }
+}
--- Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
+++ Essentail-practice/Essentail-practiceTests/Feed Presentation/FeedPresenterTest.swift
@@ -8,79 +8,7 @@
 import XCTest
 import Essentail_practice
 
-class FeedPresenter  {
-    init(errorView: FeedErrorView, loadingView: FeedLoadingView, feedView: FeedView) {
-        self.errorView = errorView
-        self.loadingView = loadingView
-        self.feedView = feedView
-        
-    }
-    
-    private let errorView: FeedErrorView
-    private let loadingView: FeedLoadingView
-    private let feedView: FeedView
-    
-    static var title: String {
-        return NSLocalizedString("FEED_VIEW_TITLE",
-                                 tableName: "Feed",
-                                 bundle: Bundle(for: FeedPresenter.self),
-                                 comment: "Title for the feed view")
-    }
-    
-    static var errorMessage: String {
-        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
-                                 tableName: "Feed",
-                                 bundle: Bundle(for: FeedPresenter.self),
-                                 comment: "load error message")
-    }
-    
-    func didStartLoadingFeed() {
-        errorView.display(.noError())
-        loadingView.display(FeedLoadingViewModelData(isLoading: true))
-    }
-    
-    func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(FeedViewModelData(feed: feed))
-        loadingView.display(FeedLoadingViewModelData(isLoading: false))
-    }
-    
-    func didFinsihLoadingFeed(with error: Error) {
-        loadingView.display(FeedLoadingViewModelData(isLoading: false))
-        errorView.display(.error(message: FeedPresenter.errorMessage))
-    }
-}
 
-struct FeedErrorViewModelData {
-    var message: String?
-    
-    static func noError() -> FeedErrorViewModelData {
-        return FeedErrorViewModelData(message: .none)
-    }
-    
-    static func error(message: String) -> FeedErrorViewModelData {
-        return FeedErrorViewModelData(message: message)
-    }
-}
-
-protocol FeedErrorView {
-    func display(_ viewModel: FeedErrorViewModelData)
-}
-
-struct FeedLoadingViewModelData {
-    let isLoading: Bool
-}
-
-protocol FeedLoadingView{
-    func display(_ viewModel: FeedLoadingViewModelData)
-}
-
-struct FeedViewModelData {
-    let feed: [FeedImage]
-}
-
-protocol FeedView {
-    func display(_ viewModel: FeedViewModelData)
-}
 
 
 final class FeedPresenterTest: XCTestCase {
@@ -120,27 +48,7 @@ final class FeedPresenterTest: XCTestCase {
     
     
     
-    private class ViewSpy: FeedErrorView,  FeedLoadingView, FeedView{
-        private(set) var messages = Set<Message>()
-        
-        enum Message: Hashable {
-            case display(errorMessage: String?)
-            case display(isLoading: Bool)
-            case display(feed: [FeedImage])
-        }
-        func display(_ viewModel: FeedErrorViewModelData) {
-            messages.insert(.display(errorMessage: viewModel.message))
-        }
-        
-        func display(_ viewModel: FeedLoadingViewModelData) {
-            messages.insert(.display(isLoading: viewModel.isLoading))
-        }
-        
-        func display(_ viewModel: FeedViewModelData) {
-            messages.insert(.display(feed: viewModel.feed))
-        }
-        
-    }
+
     
     func test_title_isLocalized() {
         XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
@@ -168,6 +76,28 @@ final class FeedPresenterTest: XCTestCase {
             XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
         }
         return value
+    }
+    
+    private class ViewSpy: FeedErrorView,  FeedLoadingView, FeedView{
+        private(set) var messages = Set<Message>()
+        
+        enum Message: Hashable {
+            case display(errorMessage: String?)
+            case display(isLoading: Bool)
+            case display(feed: [FeedImage])
+        }
+        func display(_ viewModel: FeedErrorViewModelData) {
+            messages.insert(.display(errorMessage: viewModel.message))
+        }
+        
+        func display(_ viewModel: FeedLoadingViewModelData) {
+            messages.insert(.display(isLoading: viewModel.isLoading))
+        }
+        
+        func display(_ viewModel: FeedViewModelData) {
+            messages.insert(.display(feed: viewModel.feed))
+        }
+        
     }
 
 }

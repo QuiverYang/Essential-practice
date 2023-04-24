a138b4d655346f883e33a5387912374652f8030b
Jacky Yang <m778677@yahoo.com.tw>
2023-04-24 16:10:20 +0800

    Add `switch` transformation statement with `map` and `flatMap` chain

--- Essentail-practice/Essentail-practice/Feed API/RemoteFeedImageDataLoader.swift
+++ Essentail-practice/Essentail-practice/Feed API/RemoteFeedImageDataLoader.swift
@@ -45,6 +45,7 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         let task = HTTPTaskWrapper(completion)
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
+            
             switch result{
             case .failure:
                 task.complete(with: .failure(Error.connectivity))
@@ -54,8 +55,14 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
                 } else {
                     task.complete(with: .failure(Error.invalidData))
                 }
-                
+
             }
+//            task.complete(with: result
+//                            .mapError { _ in Error.connectivity }
+//                            .flatMap { (data, response) in
+//                                let isValidResponse = response.statusCode == 200 && !data.isEmpty
+//                                return isValidResponse ? .success(data) : .failure(Error.invalidData)
+//                            })
         }
         return task
     }

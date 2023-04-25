5decb1921944a008089ac4cab0fe13aaa65f2398
Jacky Yang <m778677@yahoo.com.tw>
2023-04-25 16:45:30 +0800

    Move `CoreDataFeedStore` implementation of the `FeedImageDataStore` protocol to production

---
+++ Essentail-practice/Essentail-practice/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  Essentail-practice
+//
+//  Created by Menglin Yang on 2023/4/25.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+    
+}

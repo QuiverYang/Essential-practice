5e2957c3ddfb246c4772e5550210c83ab55b4c8e
Jacky Yang <m778677@yahoo.com.tw>
2023-04-25 16:52:46 +0800

    `CoreDataFeedStore.retrieveImageData` delivers stored data when there's an image with a matching URL in the store

--- Essentail-practice/Essentail-practice/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ Essentail-practice/Essentail-practice/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -31,4 +31,12 @@ extension ManagedFeedImage {
     var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, imageURL: url)
     }
+    
+    static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
+        let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
+        request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
+        request.returnsObjectsAsFaults = false
+        request.fetchLimit = 1
+        return try context.fetch(request).first
+    }
 }

cda02ab7d2052089ab06e9bbd1f736a1b900dea4
Jacky Yang <m778677@yahoo.com.tw>
2023-04-27 14:10:03 +0800

    `LocalFeedLoader.validateCache` succeeds on non-expired cache

--- Essentail-practice/Essentail-practiceTests/Feed Cache/ValidateCacheUseCaseTests.swift
+++ Essentail-practice/Essentail-practiceTests/Feed Cache/ValidateCacheUseCaseTests.swift
@@ -98,6 +98,17 @@ final class ValidateCacheUseCaseTest : XCTestCase {
         })
     }
     
+    func test_validateCache_succeedsOnNonExpiredCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let nonExpiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeWithRetrieval(with: feed.local, timestamp: nonExpiredTimestamp)
+        })
+    }
+    
     // Helpers:
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()

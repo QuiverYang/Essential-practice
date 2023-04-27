87697c2765f302e04e30ce32adb304b6fc8eda63
Jacky Yang <m778677@yahoo.com.tw>
2023-04-27 22:34:59 +0800

    Add EssentialApp Project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Menglin Yang on 2023/4/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

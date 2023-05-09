ca9c5e5a99a1a94c41146e30795a7b3c3f160525
Jacky Yang <m778677@yahoo.com.tw>
2023-05-09 15:42:08 +0800

    Extract `FeedLoader` test helpers into a shared protocol extension to remove duplication

--- EssentialApp/EssentialAppTests/EssentialAppTests.swift
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -10,7 +10,7 @@ import Essentail_practice
 import EssentialApp
 
 
-final class RemoteFeedLoaderWithLocalFallbackCompositesTests: XCTestCase {
+final class RemoteFeedLoaderWithLocalFallbackCompositesTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
@@ -46,24 +46,4 @@ final class RemoteFeedLoaderWithLocalFallbackCompositesTests: XCTestCase {
         return composer
     }
     
-    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedFeed), .success(expectedFeed)):
-                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-                
-            case (.failure, .failure):
-                break
-                
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-                
-        wait(for: [exp], timeout: 1.0)
-    }
 }
--- EssentialApp/EssentialAppTests/FeedLoaderSaveSideEffectTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderSaveSideEffectTests.swift
@@ -23,7 +23,7 @@ class FeedLoaderCacheDecorator: FeedLoader {
 }
 
 
-final class FeedLoaderSaveSideEffectTests: XCTestCase {
+final class FeedLoaderSaveSideEffectTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_deliversFeedOnLoaderSuccess() {
         let feed = uniqueFeed()
@@ -42,26 +42,4 @@ final class FeedLoaderSaveSideEffectTests: XCTestCase {
         expect(sut, toCompleteWith: .failure(anyNSError()))
     }
     
-    
-    // Helpers:
-    
-    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line ) {
-        
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load{ receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedFeed), .success(expectedFeed)):
-                XCTAssertEqual(receivedFeed, expectedFeed)
-            case (.failure, .failure):
-                break
-                
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-    }
 }

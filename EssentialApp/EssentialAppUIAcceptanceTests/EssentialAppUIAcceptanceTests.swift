bd94b463b7aa567a237abf4f02885eb1c8595ac4
Jacky Yang <m778677@yahoo.com.tw>
2023-05-11 10:37:38 +0800

    Reset cache in every test run to make sure tests run in a clean state and dont influence the result of other tests.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -7,26 +7,29 @@
 
 import XCTest
 
-final class EssentialAppUIAcceptanceTests: XCTestCase {
+class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
+        app.launchArguments = ["-reset"]
         app.launch()
+        
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
         XCTAssertEqual(feedCells.count, 22)
-        let feedImage = app.images.matching(identifier: "feed-image-view").firstMatch
-        XCTAssertTrue(feedImage.exists )
         
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
     }
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
+        onlineApp.launchArguments = ["-reset"]
         onlineApp.launch()
-        
+
         let offlineApp = XCUIApplication()
         offlineApp.launchArguments = ["-connectivity", "offline"]
         offlineApp.launch()
-        
+
         let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
         XCTAssertEqual(cachedFeedCells.count, 22)
         
@@ -38,9 +41,10 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         let app = XCUIApplication()
         app.launchArguments = ["-reset", "-connectivity", "offline"]
         app.launch()
-        
-        let cachedFeedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cachedFeedCells.count, 0)
+
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 0)
     }
     
 }
+

0a046a8798ec7c15d0bdb637e70efbfa70ec3aa6
Jacky Yang <m778677@yahoo.com.tw>
2023-05-16 23:05:36 +0800

    Record feed with failed image loading snapshot

--- Essentail-practice/EssentialPracticeiOSTests/FeedSnapshotTests.swift
+++ Essentail-practice/EssentialPracticeiOSTests/FeedSnapshotTests.swift
@@ -34,6 +34,15 @@ final class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+
+        sut.display(feedWithFailedImageLoading())
+
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
+    
     //MARK: - Helpers
     
     private func makeSUT() -> FeedViewController {
@@ -77,6 +86,21 @@ final class FeedSnapshotTests: XCTestCase {
                 description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
                 location: "Garth Pier",
                 image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub]{
+        return [
+            ImageStub(
+                description: nil,
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Garth Pier",
+                image: nil
             )
         ]
     }
